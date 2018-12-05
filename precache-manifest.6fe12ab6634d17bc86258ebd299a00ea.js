bb9c381b16e2dd78c7232be51e993e5d64cf870c
AZbang <azbango5@gmail.com>
2018-12-05 22:38:00 +0300

    Updates

---
+++ precache-manifest.6fe12ab6634d17bc86258ebd299a00ea.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e603c690f754c70654004a753c17b60b",
+    "url": "/vingo/static/media/HelveticaNeueCyr-Medium.e603c690.ttf"
+  },
+  {
+    "revision": "0a6835de4cdaa8535203",
+    "url": "/vingo/static/css/main.59cdf2b6.chunk.css"
+  },
+  {
+    "revision": "307781839e3e303ff49b223dc242baac",
+    "url": "/vingo/static/media/HelveticaNeueCyr-Light.30778183.ttf"
+  },
+  {
+    "revision": "056ac41ba223b70236f7",
+    "url": "/vingo/static/js/1.056ac41b.chunk.js"
+  },
+  {
+    "revision": "59aec32aa82f474ba309",
+    "url": "/vingo/static/js/runtime~main.59aec32a.js"
+  },
+  {
+    "revision": "fe7dec400453728a8dc19dd319f52258",
+    "url": "/vingo/static/media/icon.fe7dec40.png"
+  },
+  {
+    "revision": "5a41fe294813fd6711439a86ddb17061",
+    "url": "/vingo/static/media/classical-building-ios.5a41fe29.png"
+  },
+  {
+    "revision": "49f7df50127c6c026610fc93f1545b32",
+    "url": "/vingo/static/media/classical-building-android.49f7df50.png"
+  },
+  {
+    "revision": "0a6835de4cdaa8535203",
+    "url": "/vingo/static/js/main.0a6835de.chunk.js"
+  },
+  {
+    "revision": "5d3dd852e699da0191d2a0f886417fea",
+    "url": "/vingo/static/media/HelveticaNeueCyr-Heavy.5d3dd852.ttf"
+  },
+  {
+    "revision": "50475e9398305457c4ead55941b10a0f",
+    "url": "/vingo/static/media/HelveticaNeueCyr-Bold.50475e93.ttf"
+  },
+  {
+    "revision": "303f6a538da65cd9cd71f5cdd0d92125",
+    "url": "/vingo/static/media/HelveticaNeueCyr-Black.303f6a53.ttf"
+  },
+  {
+    "revision": "30111f49e3746df5024292767ab0494f",
+    "url": "/vingo/static/media/HelveticaNeueCyr-UltraLight.30111f49.ttf"
+  },
+  {
+    "revision": "afe760ffd0c1944072ba8f7c3f47b4d5",
+    "url": "/vingo/static/media/HelveticaNeueCyr-Thin.afe760ff.ttf"
+  },
+  {
+    "revision": "056ac41ba223b70236f7",
+    "url": "/vingo/static/css/1.b67f1f42.chunk.css"
+  },
+  {
+    "revision": "42d01a868dc14eaf8dbddff82e641361",
+    "url": "/vingo/index.html"
+  }
+];
